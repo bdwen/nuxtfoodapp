ce6ebc055858ac1b79eade83eb7763305dd7a85c
bdwen <cdsentro@gmail.com>
2021-06-03 16:56:42 +1000

    initial commit

---
+++ pages/Restaurants.vue
@@ -0,0 +1,20 @@
+<template>
+    <main class="container restaurant">
+        <h1>Restaurant Page</h1>
+        <AppRestaurantInfo />
+    </main>
+</template>
+
+<script>
+import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue';
+
+    export default {
+        components: {
+            AppRestaurantInfo
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
